4a83792285766a1ee00cc4e0ad5a922426c1d347
pranjalS9 <p.arudiya9@gmail.com>
2025-01-14 12:32:14 +0530

    Pranjal | TestCases - Add a test case for CommandFactory class (shouldRegisterAllCommandsTest)

--- src/test/java/com/codurance/training/commands/factory/CommandFactoryTest.java
+++ src/test/java/com/codurance/training/commands/factory/CommandFactoryTest.java
@@ -9,6 +9,7 @@ import org.junit.Test;
 import java.io.StringWriter;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
 
 public class CommandFactoryTest {
 
@@ -22,5 +23,17 @@ public class CommandFactoryTest {
         ICommand retrievedCommand = commandFactory.getCommand(CommandTypes.ADD);
 
         assertEquals(addCommand, retrievedCommand);
+    }
+
+    @Test
+    public void shouldRegisterAllCommandsTest() {
+        CommandService commandService = new CommandService(new Projects(), new StringWriter());
+        CommandFactory commandFactory = new CommandFactory();
+        commandFactory.registerAll(commandService);
+
+        assertNotNull(commandFactory.getCommand(CommandTypes.SHOW));
+        assertNotNull(commandFactory.getCommand(CommandTypes.ADD));
+        assertNotNull(commandFactory.getCommand(CommandTypes.CHECK));
+        assertNotNull(commandFactory.getCommand(CommandTypes.UNCHECK));
     }
 }
