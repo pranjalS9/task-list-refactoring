af078d6e39061f219b13aa8855a44f764808ca69
pranjalS9 <p.arudiya9@gmail.com>
2025-01-14 11:41:38 +0530

    Pranjal | TestCases - Add a test case for toggleTaskStatus (done) method of CommandService

--- src/test/java/com/codurance/training/commands/service/CommandServiceTest.java
+++ src/test/java/com/codurance/training/commands/service/CommandServiceTest.java
@@ -81,4 +81,23 @@ public class CommandServiceTest {
         String actualOutput = writer.toString().replace("\r\n", "\n");
         assertEquals(expectedOutput, actualOutput);
     }
+
+    @Test
+    public void toggleTaskStatusAsDoneTest() throws IOException {
+        StringWriter writer = new StringWriter();
+        Projects projects = new Projects();
+        String projectName = "Project-1";
+        Task task = new Task(1, "Task-1", false);
+
+        projects.addProject(projectName);
+        projects.addTask(projectName, task);
+
+        CommandService commandService = new CommandService(projects, writer);
+        commandService.toggleTaskStatus("1", true);
+        commandService.show();
+
+        String expectedOutput = "Project-1\n" + "[x] 1: Task-1\n";
+        String actualOutput = writer.toString().replace("\r\n", "\n");
+        assertEquals(expectedOutput, actualOutput);
+    }
 }
