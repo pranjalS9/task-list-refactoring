c4933e833d30cf752f8484eeedadcce6993130d4
pranjalS9 <p.arudiya9@gmail.com>
2025-01-14 12:10:06 +0530

    Pranjal | TestCases - Replace the test cases of CommandExecuterTest class witht the updated CommandExecuter class. Add a test case for execute method (executeCommandWithCorrectCommandTest)

--- src/test/java/com/codurance/training/commands/CommandExecutorTest.java
+++ src/test/java/com/codurance/training/commands/CommandExecutorTest.java
@@ -1,8 +1,8 @@
 package com.codurance.training.commands;
 
-import com.codurance.training.projects.Projects;
-import com.codurance.training.commands.service.CommandService;
 import com.codurance.training.commands.factory.CommandFactory;
+import com.codurance.training.commands.service.CommandService;
+import com.codurance.training.projects.Projects;
 import org.junit.Test;
 
 import java.io.StringWriter;
@@ -12,83 +12,16 @@ import static org.junit.Assert.assertEquals;
 public class CommandExecutorTest {
 
     @Test
-    public void executeAddProjectWithNoTaskTest() {
+    public void executeCommandWithCorrectCommandTest() {
         StringWriter writer = new StringWriter();
         CommandFactory commandFactory = new CommandFactory();
         commandFactory.registerAll(new CommandService(new Projects(), writer));
-        CommandExecutor taskList = new CommandExecutor(commandFactory);
+        CommandExecutor commandExecutor = new CommandExecutor(commandFactory);
 
-        taskList.execute("add project Project-1");
-        taskList.execute("show");
+        commandExecutor.execute("add project Project-1");
+        commandExecutor.execute("show");
 
         String expectedOutput = "Project-1\n";
-        assertEquals(expectedOutput, writer.toString());
-    }
-
-    @Test
-    public void executeAddProjectWithOneTaskTest() {
-        StringWriter writer = new StringWriter();
-        CommandFactory commandFactory = new CommandFactory();
-        commandFactory.registerAll(new CommandService(new Projects(), writer));
-        CommandExecutor taskList = new CommandExecutor(commandFactory);
-
-        taskList.execute("add project Project-1");
-        taskList.execute("add task Project-1 Task-1");
-        taskList.execute("show");
-
-        String expectedOutput = "Project-1\n" + "[ ] 1: Task-1\n";
-        String actualOutput = writer.toString().replace("\r\n", "\n");
-        assertEquals(expectedOutput, actualOutput);
-    }
-
-    @Test
-    public void executeAddProjectWithMultipleTaskTest() {
-        StringWriter writer = new StringWriter();
-        CommandFactory commandFactory = new CommandFactory();
-        commandFactory.registerAll(new CommandService(new Projects(), writer));
-        CommandExecutor taskList = new CommandExecutor(commandFactory);
-
-        taskList.execute("add project Project-1");
-        taskList.execute("add task Project-1 Task-1");
-        taskList.execute("add task Project-1 Task-2");
-        taskList.execute("show");
-
-        String expectedOutput = "Project-1\n" + "[ ] 1: Task-1\n" + "[ ] 2: Task-2\n";
-        String actualOutput = writer.toString().replace("\r\n", "\n");
-        assertEquals(expectedOutput, actualOutput);
-    }
-
-    @Test
-    public void executeAddProjectWithOneTaskAndThenCheckTest() {
-        StringWriter writer = new StringWriter();
-        CommandFactory commandFactory = new CommandFactory();
-        commandFactory.registerAll(new CommandService(new Projects(), writer));
-        CommandExecutor taskList = new CommandExecutor(commandFactory);
-
-        taskList.execute("add project Project-1");
-        taskList.execute("add task Project-1 Task-1");
-        taskList.execute("check 1");
-        taskList.execute("show");
-
-        String expectedOutput = "Project-1\n" + "[x] 1: Task-1\n";
-        String actualOutput = writer.toString().replace("\r\n", "\n");
-        assertEquals(expectedOutput, actualOutput);
-    }
-
-    @Test
-    public void executeAddProjectWithMultipleTaskThenCheckOneThenUncheckTest() {
-        StringWriter writer = new StringWriter();
-        CommandFactory commandFactory = new CommandFactory();
-        commandFactory.registerAll(new CommandService(new Projects(), writer));
-        CommandExecutor taskList = new CommandExecutor(commandFactory);
-
-        taskList.execute("add project Project-1");
-        taskList.execute("add task Project-1 Task-1");
-        taskList.execute("check 1");
-        taskList.execute("uncheck 1");
-        taskList.execute("show");
-
-        String expectedOutput = "Project-1\n" + "[ ] 1: Task-1\n";
         String actualOutput = writer.toString().replace("\r\n", "\n");
         assertEquals(expectedOutput, actualOutput);
     }
