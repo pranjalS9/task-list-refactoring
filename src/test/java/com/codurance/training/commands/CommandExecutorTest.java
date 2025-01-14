f087365222debbb1357f3de1e8bc7e33bd10ebd1
pranjalS9 <p.arudiya9@gmail.com>
2025-01-14 12:12:32 +0530

    Pranjal | TestCases - Add a test case for execute method (executeCommandWithIncorrectCommandTest)

--- src/test/java/com/codurance/training/commands/CommandExecutorTest.java
+++ src/test/java/com/codurance/training/commands/CommandExecutorTest.java
@@ -8,6 +8,7 @@ import org.junit.Test;
 import java.io.StringWriter;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertThrows;
 
 public class CommandExecutorTest {
 
@@ -24,5 +25,15 @@ public class CommandExecutorTest {
         String expectedOutput = "Project-1\n";
         String actualOutput = writer.toString().replace("\r\n", "\n");
         assertEquals(expectedOutput, actualOutput);
+    }
+
+    @Test
+    public void executeCommandWithIncorrectCommandTest() {
+        StringWriter writer = new StringWriter();
+        CommandFactory commandFactory = new CommandFactory();
+        commandFactory.registerAll(new CommandService(new Projects(), writer));
+        CommandExecutor commandExecutor = new CommandExecutor(commandFactory);
+
+        assertThrows(IllegalArgumentException.class, () -> commandExecutor.execute("test project Project-1"));
     }
 }
